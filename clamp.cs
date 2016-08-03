dcade5beb70fcef55e5433e8baad696ea1c34cda
Zealous Dev <Zealous Dev>
2016-08-03 14:51:43 +0200

    ini

---
+++ clamp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clamp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, fwd, out hit)) {
+			Debug.DrawRay (transform.position, fwd, Color.green);
+		}
+	}
+}
